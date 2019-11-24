1c567f8338b691fe092ad6704c892809fbfff435
jcolombo <jcolombo@360psg.com>
2019-11-23 19:26:35 -0500

    Updated for typescript and pre-compiled distribution

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,17 @@
 
-const paymoApp = {
-  x: 1
+interface xx {
+  a:number
 }
+
+const xxx = (a:xx) : number=>{
+  return a.a * 100
+}
+
+const paymoApp = {
+  x: 3,
+  y: xxx({a:5})
+}
+
+
 
 export default paymoApp
